a5e2aa67135afde99cc55bfab9cb8c8783a71425
ajirogbagoma <oghenetegaangela@gmail.com>
2024-04-09 06:48:19 +0000

    2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,13 +1,9 @@
 #!/usr/bin/node
-/*
-Script that prints a new message
-depending on the number of argument passed
- */
-const noArgs = process.argv.length;
-if (noArgs < 3) {
-  console.log('No argument');
-} else if (noArgs === 3) {
+const argc = process.argv.length;
+if (argc > 3) {
+  console.log('Arguments found');
+} else if (argc === 3) {
   console.log('Argument found');
 } else {
-  console.log('Arguments found');
+  console.log('No argument');
 }
